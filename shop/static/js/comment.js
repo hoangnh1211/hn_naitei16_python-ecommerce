abc50669a418c4f3004120b02e7764bf67b20078
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-09-09 14:34:04 +0700

    Thông kê comment

--- shop/static/js/comment.js
+++ shop/static/js/comment.js
@@ -8,29 +8,35 @@ $(document).ready(function () {
             url: `/add_comment/${product_id}`,
             data: formData,
             success: function(res) {
-                var urlt = `/delete_comment/${res.id}`;
+                $('#starmedium')[0].innerHTML = res.medium
+                $(`#totalcmt`)[0].innerHTML = res.total_comment
+                for (let i=0; i < res.classify_comment.length; i++){
+                    $(`#rate${i+1} span`)[0].innerHTML = res.classify_comment[i].dcount
+                    $(`#rate${i+1} .progress-bar`).css("width",res.classify_comment[i].style)
+                }
+                var urlt = `/delete_comment/${res.data_cmt.id}`;
                 var rate = ""
                 let result = [1,2,3,4,5].map(value=>{
-                    let status = ( res.rate < value ) ?  'fa fa-star-o empty' :  'fa fa-star'
+                    let status = ( res.data_cmt.rate < value ) ?  'fa fa-star-o empty' :  'fa fa-star'
                     rate += `<i class="${status}"> </i>`
                 })
                 var _html=
                 `<div class="single-review">
                     <div class="review-heading">
-                        <div><strong>${res.user}</strong> <i class="fa fa-clock-o"></i> ${res.create_at}</div>
+                        <div><strong>${res.data_cmt.user}</strong> <i class="fa fa-clock-o"></i> ${res.data_cmt.create_at}</div>
                         <div class="review-rating pull-right">
                         ${rate}
                         </div>
                     </div>
                     <div class="review-body">
-                        ${res.content}
+                        ${res.data_cmt.content}
                         <div class="review-rating pull-right">
                             <a href=${urlt}><i class="fa fa-trash-o" aria-hidden="true"></i></a>
                         </div>
                     </div>
                 </div>
                 <hr>`;
-                if (res.id)
+                if (res.data_cmt.id)
                     $(".comment-wrapper").prepend(_html);
                 $('input[type=text], textarea').val('');
                 $('input[type=radio]').prop("checked", false);
